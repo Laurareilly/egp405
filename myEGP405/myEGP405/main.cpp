2d852fe6f3948fa349f78605c4a670d1d64aaad7
laurareilly <laura.reilly@mymail.champlain.edu>
2017-08-31 19:50:36 -0400

    set up raknet creating a server, client, and destroying the instance

--- myEGP405/myEGP405/main.cpp
+++ myEGP405/myEGP405/main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <stdio.h>
+#include "../../../DevSDKs/DevSDKs/include/RakNet/RakPeerInterface.h"
 
 using namespace std;
+using namespace RakNet;
 
 int main()
 {
+	unsigned int maxClients = 10;
+	unsigned short serverPort = 60000;
+	char str[512];
+	RakNet::RakPeerInterface *peer = RakNet::RakPeerInterface::GetInstance();
+	bool isServer;
+
+	cout << "(C) or (S)erver?\n";
+	cin >> str;
+
+	cout << "Enter the server port number\n";
+	cin >> serverPort;
+
+	if (str[0] == 'c' || str[0] == 'C') //if we're starting a client
+	{
+		RakNet::SocketDescriptor sd;
+		peer->Startup(1, &sd, 1);
+		isServer = false;
+	}
+	else //if we're starting a server
+	{
+		cout << "Enter the max amount of clients\n";
+		cin >> maxClients;
+		RakNet::SocketDescriptor sd(serverPort, 0);
+		peer->Startup(maxClients, &sd, 1);
+		isServer = true;
+	}
+
+	RakNet::RakPeerInterface::DestroyInstance(peer);
 
 	cin.get();
 	cin.ignore();
